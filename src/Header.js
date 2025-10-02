f28e03ec524d3262afc4c4804f7cbc940c655393
sahara <saharina123y@gmail.com>
2025-10-02 14:43:57 +0300

    done

---
+++ src/Header.js
@@ -0,0 +1,14 @@
+
+
+function Header({ isDarkMode, onDarkModeClick }) {
+  return (
+    <header>
+      <h1>Grocery List</h1>
+      <button onClick={onDarkModeClick}>
+        {isDarkMode ? "Light Mode" : "Dark Mode"}
+      </button>
+    </header>
+  );
+}
+
+export default Header;
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,29 +1,27 @@
 import React, { useState } from "react";
 import Item from "./Item";
 
-function ShoppingList({ items }) {
+import itemData from "../data/items";
+
+function ShoppingList() {
   const [selectedCategory, setSelectedCategory] = useState("All");
 
   function handleCategoryChange(event) {
     setSelectedCategory(event.target.value);
   }
 
-  const itemsToDisplay = items.filter((item) => {
+  const itemsToDisplay = itemData.filter((item) => {
     if (selectedCategory === "All") return true;
-
     return item.category === selectedCategory;
   });
 
   return (
     <div className="ShoppingList">
-      <div className="Filter">
-        <select name="filter" onChange={handleCategoryChange}>
-          <option value="All">Filter by category</option>
-          <option value="Produce">Produce</option>
-          <option value="Dairy">Dairy</option>
-          <option value="Dessert">Dessert</option>
-        </select>
-      </div>
+      <Filter
+        selectedCategory={selectedCategory}
+        onCategoryChange={handleCategoryChange}
+      />
+
       <ul className="Items">
         {itemsToDisplay.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
